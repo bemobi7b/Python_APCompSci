3e89a1981aa616a3d04c80ee9c9752ee6ec78a97
Bemobi7b <bemobi7b@gmail.com>
2025-01-10 10:38:50 -0600

    Kevin Bacon Game with env + gitignore

--- # the 7 Degrees of Kevin Bacon game.py
+++ # the 7 Degrees of Kevin Bacon game.py
@@ -1,6 +1,13 @@
 # the 7 Degrees of Kevin Bacon game
-# import requests
+import requests
+import json
 import csv
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.environ.get("API_KEY")
 
 allCountries1 = ["United States", "Canada", "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "North Korea", "South Korea", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Eswatini", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Democratic Republic of the Congo", "Zambia", "Zimbabwe"]
 
@@ -41,30 +48,31 @@ def remove_items(test_list, item):
 #https://www.analyticsvidhya.com/blog/2021/08/python-tutorial-working-with-csv-file-for-data-science/
 # I used this website to learn how to traverse .csv files 
 # The next line opens the file with a list of movies and their performance on the Bechdel Test among other data
-# It also puts the file in read mode ('r') and keeps it open until 
-with open('bechdelTest.csv', 'r') as csvfile:
-  # Create a reader object
-  csv_reader = csv.reader(csvfile)
-  for row in csv_reader:
-      moviesBechdelList.append(row[2])
-      passFailsList.append(row[3])
-      movieYearsBechdelList.append(row[1])
+# It also puts the file in read mode ('r') and keeps it open
 
-with open('netflixContent.csv', 'r') as csvfile:
-   csv_reader = csv.reader(csvfile)
-   for row in csv_reader:
+# with open('bechdelTest.csv', 'r', encoding="utf16") as csvfile:
+#   # Create a reader object
+#   csv_reader = csv.reader(csvfile)
+#   for row in csv_reader:
+#       moviesBechdelList.append(row[2])
+#       passFailsList.append(row[3])
+#       movieYearsBechdelList.append(row[1])
+
+with open('netflixContent.csv', 'r', encoding="utf8") as csvfile:
+   csv_reader2 = csv.reader(csvfile)
+   for row in csv_reader2:
       netflixContentList.append(row[2])
       netflixCountryList.append(row[3])
       if row[0] != "id":
          netflixReleaseYearsList.append(float(row[5]))
 
-for count in range(len(netflixReleaseYearsList) - 1):
+for count in range(len(netflixReleaseYearsList)):
    netflixReleaseYearsList[count] = int(netflixReleaseYearsList[count])
 #converts all years into int values because they had a .0 at the end in the csv file
 
 with open('imdbTopThousandMovies.csv', 'r', encoding="utf8") as csvfile:
-  csv_reader2 = csv.reader(csvfile)
-  for row in csv_reader2:
+  csv_reader3 = csv.reader(csvfile)
+  for row in csv_reader3:
       topMoviesList.append(row[2])
       topMoviesStar1.append(row[11])
       topMoviesStar2.append(row[12])
@@ -93,7 +101,9 @@ coStarsOfBacon = remove_items(coStarsWithBacon, "Kevin Bacon")
 
 print(baconMoviesfromTop)
 print(coStarsOfBacon)
-print(netflixReleaseYearsList[4])
+# print(netflixReleaseYearsList)
+
+print(API_KEY)
 
 # Kevin bacon is in the 1994 film, The River Wild, on Netflix
 
